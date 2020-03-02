b85e5eaf7d40f9309132ea120a230f6fbd875c98
nicholasbvolpe <41712102+nicholasbvolpe@users.noreply.github.com>
2020-03-02 16:54:29 -0700

    Update OptionsDecorator.java

--- OptionsDecorator.java
+++ OptionsDecorator.java
@@ -1,4 +1,10 @@
 public abstract class OptionsDecorator extends Car {
+    /*
+        inUse: the number of options in use on the car
+        (1 or 0 for GPS and Satellite Radio, 0 to 4 for car seat)
+        
+        The cost function is overridden by each option class, since they don't have the same prices.
+    */
     abstract int cost(Car car);
     int inUse;
 }
