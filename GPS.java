77ccf393f99200574f3094b01bb2b8e87d81650b
nicholasbvolpe <41712102+nicholasbvolpe@users.noreply.github.com>
2020-03-03 13:06:11 -0700

    Update GPS.java

--- GPS.java
+++ GPS.java
@@ -1,24 +1,24 @@
 public class GPS extends OptionsDecorator {
-    public selected;
+    public Car selected;
 
     void GPS(Car theCar) {
         this.selected = theCar;
     }
 
-    int cost(Car car) {
-        if (car instanceof EconomyCar) {
+    int cost() {
+        if (this.selected instanceof EconomyCar) {
             return this.selected.getPrice() + 10;
         }
-        else if (car instanceof LuxuryCar) {
+        else if (this.selected instanceof LuxuryCar) {
             return this.selected.getCost() + 40;
         }
-        else if (car instanceof SUVCar) {
+        else if (this.selected instanceof SUVCar) {
             return this.selected.getCost() + 30;
         }
-        else if (car instanceof StandardCar) {
+        else if (this.selected instanceof StandardCar) {
             return this.selected.getCost() + 20;
         }
-        else if (car instanceof MinivanCar) {
+        else if (this.selected instanceof MinivanCar) {
             return this.selected.getCost() + 20;
         }
         else {
